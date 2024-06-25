05f5c4b25fed85163a7642a713e7c73fcd6bf68a
Steve Rosam <steve+quix-org@quix.io>
2024-06-25 11:11:55 +0000

    Edited 'main.py'

--- web-api-gateway/main.py
+++ web-api-gateway/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from quixstreams import Application
+from waitress import serve
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -28,3 +29,4 @@ def publish_event():
 
 if __name__ == '__main__':
     app.run(debug=True, port=80)
+    serve(app, host="0.0.0.0", port=80)
